c740c8233a8a821a46c3bf44be00ed3b35002b7d
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-02-09 17:48:41 +0100

    spring initializr template for the api

---
+++ src/test/java/com/doogod/video/meetingapi/MeetingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.doogod.video.meetingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
