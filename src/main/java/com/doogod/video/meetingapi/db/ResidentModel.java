173eb7882a926d532efa7e85844acfae73f5ee0d
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-03-15 21:27:32 +0100

    add resident model

---
+++ src/main/java/com/doogod/video/meetingapi/db/ResidentModel.java
@@ -0,0 +1,35 @@
+package com.doogod.video.meetingapi.db;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializable;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
+
+import java.beans.ConstructorProperties;
+
+public class ResidentModel {
+    public String name;
+    public int id;
+
+    @ConstructorProperties({"id", "name"})
+    public ResidentModel(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
