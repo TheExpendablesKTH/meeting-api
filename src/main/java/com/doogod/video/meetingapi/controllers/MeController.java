b3386f47066347074d93a28832d249bada6df93d
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-04-11 20:50:33 +0200

    add permissions to response in /me

--- src/main/java/com/doogod/video/meetingapi/controllers/MeController.java
+++ src/main/java/com/doogod/video/meetingapi/controllers/MeController.java
@@ -3,7 +3,9 @@ package com.doogod.video.meetingapi.controllers;
 import com.doogod.video.meetingapi.db.exceptions.IdentityNotFoundException;
 import com.doogod.video.meetingapi.db.models.Identity;
 import com.doogod.video.meetingapi.security.authentication.AuthenticationService;
+import com.doogod.video.meetingapi.security.permissions.Permissions;
 import org.jdbi.v3.core.Jdbi;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -23,16 +25,22 @@ public class MeController {
 
     Identity identity;
 
+    Permissions permissions;
+
     @RequestMapping(path = "", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Identity> listUsers(@RequestHeader("Authorization") String auth) {
+    public ResponseEntity<String> listUsers(@RequestHeader("Authorization") String auth) {
         try {
             identity = authService.findByToken(auth);
+            permissions = authService.parsePermissions(auth);
         } catch (IdentityNotFoundException e) {
-            org.json.JSONObject response = new org.json.JSONObject();
-            response.put("message", "unauthorized");
-            return new ResponseEntity(response.toString(), HttpStatus.FORBIDDEN);
+            return new ResponseEntity(new JSONObject().put("message", "unauthorized").toString(), HttpStatus.UNAUTHORIZED);
         }
 
-        return new ResponseEntity(identity, HttpStatus.OK);
+        JSONObject response = new JSONObject();
+        response.put("identity", new JSONObject(identity));
+        response.put("permissions", permissions.toArray());
+        response.put("token", auth.replace("Bearer ", ""));
+
+        return new ResponseEntity(response.toString(), HttpStatus.OK);
     }
 }
