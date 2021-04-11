3b12ae49a2a1ee2a4e39b6b2744f2af66c7195ec
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-04-11 16:48:30 +0200

    add admin and identities table

--- database/schema.sql
+++ database/schema.sql
@@ -1,23 +1,160 @@
--- -------------------------------------------------------------
--- TablePlus 3.7.1(332)
 --
--- https://tableplus.com/
+-- PostgreSQL database dump
 --
--- Database: meetings
--- Generation Time: 2021-03-15 21:21:03.2830
--- -------------------------------------------------------------
 
+-- Dumped from database version 13.2
+-- Dumped by pg_dump version 13.2
 
-DROP TABLE IF EXISTS "public"."residents";
--- This script only contains the table creation statements and does not fully represent the table in the database. It's still missing: indices, triggers. Do not use it as a backup.
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
 
--- Sequence and defined type
-CREATE SEQUENCE IF NOT EXISTS residents_id_seq;
+SET default_tablespace = '';
 
--- Table Definition
-CREATE TABLE "public"."residents" (
-    "id" int4 NOT NULL DEFAULT nextval('residents_id_seq'::regclass),
-    "name" varchar NOT NULL,
-    PRIMARY KEY ("id")
+SET default_table_access_method = heap;
+
+--
+-- Name: admins; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.admins (
+    id integer NOT NULL,
+    name character varying
 );
+
+
+ALTER TABLE public.admins OWNER TO postgres;
+
+--
+-- Name: admins_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.admins_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.admins_id_seq OWNER TO postgres;
+
+--
+-- Name: admins_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE public.admins_id_seq OWNED BY public.admins.id;
+
+
+--
+-- Name: identities_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.identities_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.identities_id_seq OWNER TO postgres;
+
+--
+-- Name: identities; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.identities (
+    id integer DEFAULT nextval('public.identities_id_seq'::regclass) NOT NULL,
+    username character varying NOT NULL,
+    password character varying NOT NULL,
+    admin_id integer
+);
+
+
+ALTER TABLE public.identities OWNER TO postgres;
+
+--
+-- Name: residents_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE public.residents_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.residents_id_seq OWNER TO postgres;
+
+--
+-- Name: residents; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.residents (
+    id integer DEFAULT nextval('public.residents_id_seq'::regclass) NOT NULL,
+    name character varying NOT NULL
+);
+
+
+ALTER TABLE public.residents OWNER TO postgres;
+
+--
+-- Name: admins id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.admins ALTER COLUMN id SET DEFAULT nextval('public.admins_id_seq'::regclass);
+
+
+--
+-- Name: admins admins_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.admins
+    ADD CONSTRAINT admins_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: identities identities_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.identities
+    ADD CONSTRAINT identities_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: residents residents_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.residents
+    ADD CONSTRAINT residents_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: identities_username_unique; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX identities_username_unique ON public.identities USING btree (username);
+
+
+--
+-- Name: identities identities_admin_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.identities
+    ADD CONSTRAINT identities_admin_id_fkey FOREIGN KEY (admin_id) REFERENCES public.admins(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
 
