9e16810bbe8f0f988884e44b68c7f42f2564a083
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-04-11 17:02:47 +0200

    update auth controller with authentication method for admins

--- src/main/java/com/doogod/video/meetingapi/controllers/AuthController.java
+++ src/main/java/com/doogod/video/meetingapi/controllers/AuthController.java
@@ -1,18 +1,30 @@
 package com.doogod.video.meetingapi.controllers;
 
+import com.doogod.video.meetingapi.db.exceptions.IdentityNotFoundException;
+import com.doogod.video.meetingapi.db.models.Admin;
+import com.doogod.video.meetingapi.db.services.AdminService;
+import com.doogod.video.meetingapi.security.authentication.AuthenticationService;
+import org.jdbi.v3.core.Jdbi;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/authenticate")
 @CrossOrigin(origins = "*")
 public class AuthController {
+
+    @Autowired
+    Jdbi jdbi;
+
+    @Autowired
+    AdminService adminService;
+
+    @Autowired
+    AuthenticationService authService;
 
     @RequestMapping(path = "/device", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> device() {
@@ -59,14 +71,20 @@ public class AuthController {
     }
 
     @RequestMapping(path = "/admin", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> admin() {
-        JSONObject auth = new JSONObject();
-        auth.put("token", "some-auth-token");
-
-        JSONObject response = new JSONObject();
-        response.put("message", "credentials correct");
-        response.put("auth", auth);
-
-        return new ResponseEntity<String>(response.toString(), HttpStatus.OK);
+    @PostMapping(
+            value = "/postbody",
+            consumes = {MediaType.APPLICATION_JSON_VALUE},
+            produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
+    public ResponseEntity<String> admin(@RequestBody Admin admin) {
+        try {
+            JSONObject response = new JSONObject();
+            response.put("token", authService.login(admin.getIdentity()));
+            return new ResponseEntity<String>(response.toString(), HttpStatus.OK);
+        } catch (IdentityNotFoundException e) {
+            JSONObject response = new JSONObject();
+            response.put("message", e.getClass().getSimpleName());
+            return new ResponseEntity(response.toString(), HttpStatus.FORBIDDEN);
+        }
     }
 }
