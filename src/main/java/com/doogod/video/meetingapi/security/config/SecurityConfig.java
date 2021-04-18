13bf35661aaf8781d82dd15e9e4342456f41c41b
Ludwig Kristoffersson <ludwig@kristoffersson.org>
2021-04-18 16:38:15 +0200

    fix issue where authentication broke cors

--- src/main/java/com/doogod/video/meetingapi/security/config/SecurityConfig.java
+++ src/main/java/com/doogod/video/meetingapi/security/config/SecurityConfig.java
@@ -68,6 +68,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http
+                .cors()
+                .and()
                 .sessionManagement()
                 .sessionCreationPolicy(STATELESS)
                 .and()
